bc8b8ecec3b5afd8d5ac8dfc12792748e4480ae3
shin5ok <shin5ok@55mp.com>
2022-06-09 18:38:07 +0900

    make some values use variables

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,6 @@
 gcloud iam service-accounts create tweets
-SA=$(gcloud iam service-accounts list --format=json | jq '.[].email|select(test("tweets"))' -r)
-echo $SA
-gcloud projects add-iam-policy-binding --member=serviceAccount:tweets@shingo-ar-twittertest.iam.gserviceaccount.com shingo-ar-twittertest --role=roles/pubsub.publisher
+RUN_SA=$(gcloud iam service-accounts list --format=json | jq '.[].email|select(test("tweets"))' -r)
+gcloud projects add-iam-policy-binding --member=serviceAccount:$RUN_SA $PROJECT --role=roles/pubsub.publisher
 
 gcloud pubsub topics create tweets
 gcloud pubsub subscriptions create tweets --topic=tweets
